f06731dd6ea2b94bb43cd9e701eee0e0be52b127
David Nguyen <vi.nguyen@mum.edu>
2019-08-03 22:42:39 -0500

    Lab9

---
+++ Lab9/lab/src/main/java/edu/mum/employeerestful/EmployeeRestfulApplication.java
@@ -0,0 +1,16 @@
+package edu.mum.employeerestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@SpringBootApplication
+public class EmployeeRestfulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeRestfulApplication.class, args);
+    }
+}
